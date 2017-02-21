d762c26d80db1b52d611d9190de1fbf15c8a8411
Ricardo Contreras <ricardocontrerasrobles@gmail.com>
2017-02-20 20:13:24 -0800

    Initial Commit

---
+++ AlgorithmsAndDataStructures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AlgorithmsAndDataStructures
+//
+//  Created by Ricardo Contreras on 2/20/17.
+//  Copyright © 2017 rcr. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
